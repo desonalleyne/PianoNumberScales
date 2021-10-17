885aa849dccb3654afe15dc07f871abeda238b6e
Deson <desonalleyne@gmail.com>
2021-10-17 19:21:54 -0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from scales import scales
 scaleLetter = None
 num = None
 reps = 48
-print(os.name)
+
 if len(sys.argv) > 2:
     # delay between flashes
     num = int(sys.argv[1]) or 2
@@ -20,34 +20,39 @@ def mainMenu(scaleLetter, num, reps):
         scaleLetter=input("Please enter a scale. eg: g# or ab or a: ").title()
         num=float(input("Please enter flash interval: "))
         reps=int(input("Please enter number of reps or press enter to continue: "))
+        degrees=int(input("Please enter number of keys to use: "))
         reps = 99 if reps == 0 else reps
+        
+    for lvl in range(degrees,8):
+        if lvl == 5:
+            reps = 49 
+        # selected scale to practice
+        scale = scales[scaleLetter]
+        if os.name == "posix":
+            os.system("clear")
+        else:
+            os.system("cls")
+        print(f"Flashing keys in the {scaleLetter} scale")
+        print(f"    Interval: {num}s")
+        print(f"    Reps: {reps}")
+        print(f"    Degree: {lvl}")
+        print("    Notes: " + ", ".join(scale[0:8]))
+        print("===================")
+        print()
+        print()
 
-
-    # selected scale to practice
-    scale = scales[scaleLetter]
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
-    print(f"Flashing keys in the {scaleLetter} scale")
-    print(f"Interval: {num}s")
-    print(f"Reps: {reps}")
-    print("===================")
-    print()
-    print()
-
-    print('        Position:  Key')
-    for r in range(1,8):
-        print('   {} tone of {}:  {}'.format(ordinal(r), scaleLetter, scale[r-1]) )
-    input("Press enter to continue. ")
-    print()
-    print()
-    for i in range(reps):
-        r=randint(1,7)
-        print('   {} tone of {}:  '.format(ordinal(r), scaleLetter), end=' ', flush=True)
-        sleep(num)
-        print(scale[r-1])
-        sleep(1)
+        print('        Position:  Key')
+        for r in range(1,lvl+1):
+            print('   {} tone of {}:  {}'.format(ordinal(r), scaleLetter, scale[r-1]) )
+        input("Press enter to continue... ")
+        print()
+        print()
+        for i in range(reps):
+            r=randint(1,lvl)
+            print('   {} tone of {}:  '.format(ordinal(r), scaleLetter), end=' ', flush=True)
+            sleep(num)
+            print(scale[r-1])
+            sleep(1)
     mainMenu(scaleLetter, num, reps)
 
 mainMenu(scaleLetter, num, reps)
